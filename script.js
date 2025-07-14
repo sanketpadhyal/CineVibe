4324c9dea2fba1071d72cd368568e1324561bf48
VortexByte <mrsanketpadhyal@gmail.com>
2025-07-15 03:12:25 +0530

    Update script.js
    JUST REMOVED MY API FOR SECURITY PURPOSES ADD YOUR OWN API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1660e7ee4192268a91879797d1be0f79";
+const API_KEY = "Enter Your Api Here";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const COUNTRY = "IN";
